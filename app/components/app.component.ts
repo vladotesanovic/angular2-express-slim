63b254807b22623fa2d9deb1b97aa3780ad347df
Vlado Tesanovic <vlado.tesanovic@gmail.com>
2016-02-22 15:14:20 +0100

    components folder with html and css

---
+++ app/components/app.component.ts
@@ -0,0 +1,9 @@
+import {Component} from 'angular2/core';
+@Component({
+    selector: 'my-app',
+    templateUrl: 'components/app.component.html',
+    styleUrls: ['components/app.component.css']
+})
+export class AppComponent {
+    name: string = "Angular 2";
+}
