a50ce3a97fc1cec34da9faf80898c6e96fe3475a
jrgleason <jackiegleason@gmail.com>
2016-05-21 18:04:03 -0400

    Getting more gulp working now we are copying over the dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,30 @@ var gulp = require('gulp'),
     gulpCopy       = require('gulp-copy'),
     runSequence    = require('run-sequence'),
     clean          = require('gulp-clean'),
+    mainBowerFiles = require('main-bower-files'),
+    debug          = require('gulp-debug'),
+    install        = require('gulp-install'),
     embedTemplates = require('gulp-angular-embed-templates');
 
 gulp.task('default', function(pipe){
-   runSequence('clean', 'build', pipe); 
+   runSequence('clean', 'install', 'build', pipe); 
 });
 
-gulp.task('build', ['js:build', 'css:build']);
+gulp.task('install', ['bower:install']);
+
+gulp.task('bower:install', function(){
+    return gulp.src(['./bower.json'])
+               .pipe(install());
+});
+
+gulp.task('build', ['js:build', 'css:build', 'dependencies:build']);
+
+gulp.task('dependencies:build', function(){
+    // TODO: I would like to add a flattening here for now ok
+  return gulp.src(mainBowerFiles())
+        // .pipe(debug())
+        .pipe(gulpCopy('public/deps'))
+});
 
 gulp.task('js:build', function () {
     var tsProject = ts.createProject('tsconfig.json');
@@ -28,6 +45,6 @@ gulp.task('css:build', function(){
               }))
 });
 gulp.task('clean', function(){
-   return gulp.src(['public/ng2'], {read: false})
+   return gulp.src(['public/ng2', 'public/deps', 'bower_components'], {read: false})
               .pipe(clean());
 });
